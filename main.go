c67a884c68fd584b960161c526453df0faddc69e
Richard Bailey <richard@richardbailey.xyz>
2019-07-15 23:57:30 +0100

    random initial board

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
-import "fmt"
-
-type board struct {
-	width int
-	height int
-}
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-
-	b := new(board)
-	b.deadState(6,5)
-	fmt.Println(b)
+	a := randomState(100, 100)
+	printBoard(a)
 }
 
-func (b *board) deadState(w, h int ) {
-	b.width = w
-	b.height = h
+
+func printBoard(b [][]int) {
+
+	fmt.Print(string('|'))
+	for x := 1; x <= len(b[0]); x++ {
+		fmt.Print(string('-'))
+	}
+	fmt.Println(string('|'))
+
+	for y := 0; y < len(b); y++ {
+		fmt.Print(string('|'))
+		for x := 0; x < len(b[y]); x++ {
+			if b[y][x] == 1 {
+				fmt.Print(string('#'))
+			} else {
+				fmt.Print(string(' '))
+			}
+		}
+		fmt.Println(string('|'))
+	}
+
+	fmt.Print(string('|'))
+	for x := 1; x <= len(b[0]); x++ {
+		fmt.Print(string('-'))
+	}
+	fmt.Println(string('|'))
 }
 
+func randomState(w, h int) (a [][]int) {
+	var b []int
+
+
+	for j := 0; j < h; j++ {
+
+		for i := 0; i < w; i++ {
+
+			s := rand.NewSource(time.Now().UnixNano())
+			r := rand.New(s)
+			b = append(b, r.Intn(2))
+		}
+
+		a = append(a, b)
+		b = []int{}
+	}
+
+	return a
+}
